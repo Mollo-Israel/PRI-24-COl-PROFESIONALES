e02fffdb7b33854a1e055f8e163124a871737fa0
jugodenaranja3 <vrr0032855@est.univalle.edu>
2024-11-28 19:33:37 -0400

    docker

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM mcr.microsoft.com/dotnet/aspnet:6.0 AS base
 WORKDIR /app
 EXPOSE 80
-
+ 
 # Use the official SDK image to build the application
 FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
 WORKDIR /src
@@ -11,11 +11,11 @@ RUN dotnet restore "ProyectoColProfesionales/ProyectoColProfesionales.csproj"
 COPY ProyectoColProfesionales/ ProyectoColProfesionales/
 WORKDIR "/src/ProyectoColProfesionales"
 RUN dotnet build "ProyectoColProfesionales.csproj" -c Release -o /app/build
-
+ 
 # Publish the application
 FROM build AS publish
 RUN dotnet publish "ProyectoColProfesionales.csproj" -c Release -o /app/publish
-
+ 
 # Use the base image to run the application
 FROM base AS final
 WORKDIR /app
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
-
+ 
 # Start SQL Server in the background
 /opt/mssql/bin/sqlservr &
-
+ 
 # Wait for SQL Server to start
 echo "Waiting for SQL Server to start..."
 until /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -Q "SELECT 1" &>/dev/null
 do
   sleep 1
 done
-
+ 
 # Debug: List contents of /migration directory
 echo "Listing /migration directory contents:"
 ls -l /migration
-
+ 
 # Run the migration script
 echo "Running migration script..."
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i /migration/db.sql
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d DBColProfessional -i /migration/migration.sql
-
+ 
+if /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i /migration/db.sql; then
+  echo "db.sql executed successfully."
+else
+  echo "Error executing db.sql" >&2
+  exit 1
+fi
+ 
+DB_NAME="DBColProfessional"
+if /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d $DB_NAME -i /migration/migration.sql; then
+  echo "migration.sql executed successfully."
+else
+  echo "Error executing migration.sql" >&2
+  exit 1
+fi
+ 
+# Wait for all background processes
 wait
